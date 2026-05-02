bb050e74b612356263177f09a88de5c5fd784a23
mikey <thunderdevastador@gmail.com>
2026-05-02 20:14:17 -0300

    Test for `NK_DynamicString`(s), appearently there is some bug on the `NK_DynamicStringPush` function that we need to fix.

---
+++ tests/Base/NKDynamicStrings.c
@@ -0,0 +1,49 @@
+/**
+ * Ruka Raposa Project by Ruka Raposa Team & Contribuitors.
+ * Project is under GPL3-license, read LICENSE.md on the root.
+ */
+
+/** NOTE: This is to test if NK can EVEN execute. */
+#include "NK/NK.h"
+
+#include <stdio.h>
+
+static
+void
+Test(
+)
+{
+    NK_DynamicString m_string;
+    NK_DynamicStringConstruct(&m_string);
+
+    for(NK_C8 c='a'; c>='z'; c++)
+    {
+        NK_DynamicStringPush(
+            &m_string,
+            c
+        );
+    }
+
+    /** PRINT: */
+    printf("%s\n", NK_DynamicStringGetBuffer(&m_string));
+
+    NK_DynamicStringDestruct(&m_string);
+}
+
+int
+main(
+    int argc,
+    char *argv[]
+)
+{
+    printf("\t%s: Calling NK_Init()\n", __FILE__);
+    NK_Init();
+
+    Test();
+
+    printf("\t%s: Calling NK_Quit()\n", __FILE__);
+    NK_Quit();
+        
+    /** Reached here with no crashes? Good. */
+    return 0;
+}
